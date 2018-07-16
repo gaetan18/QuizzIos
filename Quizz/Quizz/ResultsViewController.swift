55ad7d7e16b5a8e9d9646fafb537eebc96cec59b
Juan <juan@juans-mac.local>
2018-07-16 14:06:55 +0200

    [ADD] Affichage du recepa des scores finaux et gestion des égalités

--- Quizz/Quizz/ResultsViewController.swift
+++ Quizz/Quizz/ResultsViewController.swift
@@ -5,6 +5,8 @@ class ResultsViewController: UIViewController {
     var players : [Player]!
     var winnerPlayer : Player!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultPlayer1: UILabel!
+    @IBOutlet weak var resultPlayer2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,8 +15,14 @@ class ResultsViewController: UIViewController {
             if(player.nbCorrectAnswer > winnerPlayer.nbCorrectAnswer){
                 winnerPlayer = player
             }
+        }
+        if(players[0].nbCorrectAnswer == players[1].nbCorrectAnswer){
+            resultLabel.text = "Vous gagnez tout les deux !"
+        } else {
             resultLabel.text = "\(winnerPlayer.name) gagne !"
         }
+        resultPlayer1.text = "\(players[0].name) a un score de \(players[0].nbCorrectAnswer)"
+        resultPlayer2.text = "\(players[1].name) a un score de \(players[1].nbCorrectAnswer)"
     }
     @IBAction func reDo(_ sender: UIButton) {
          performSegue(withIdentifier: "sgMenu", sender: nil)
